926770b89b8593b742d9b6e6a7c8578eaae1afbb
DownTheMatrix <suija87@gmail.com>
2021-02-23 12:28:05 +0100

    Update assets and styles

--- js/main.js
+++ js/main.js
@@ -13,49 +13,60 @@ gsap.set(TRAIN, {
 });
 
 gsap.set(CLOUD_LARGE, {
-    x: "-500px"
-})
+  x: "-500px",
+});
 
 gsap.set(CLOUD_MEDIUM, {
-    x: "800px"
-})
+  x: "800px",
+});
 
 gsap.set(CLOUD_SMALL, {
-    x: "600px"
-})
+  x: "600px",
+});
 
 gsap.set(CAR_BLUE, {
-    x: "300px"
-})
+  x: "300px",
+});
+
+gsap.set(CAR_YELLOW, {
+  x: "-100px",
+});
 
 /* Animate values */
-/* gsap.to(CAR_BLUE, {
-    repeat: -1,
-    duration: 10,
-    ease: "linear",
-    x: "-1800px",
-}) */
+gsap.to(CAR_BLUE, {
+  repeat: -1,
+  duration: 10,
+  ease: "linear",
+  x: "-1800px",
+});
+
+gsap.to(CAR_YELLOW, {
+  repeat: -1,
+  duration: 10,
+  ease: "linear",
+  x: "2000px",
+});
 
 gsap.to(CLOUD_LARGE, {
-    repeat: -1,
-    duration: 60,
-    ease: "linear",
-    x: "1800px",
-})
+  repeat: -1,
+  duration: 60,
+  ease: "linear",
+  x: "1800px",
+});
 
 gsap.to(CLOUD_MEDIUM, {
-    repeat: -1,
-    duration: 50,
-    ease: "linear",
-    x: "-2000px"
-})
+  repeat: -1,
+  duration: 50,
+  ease: "linear",
+  x: "-2000px",
+});
 
 gsap.to(CLOUD_SMALL, {
-    repeat: -1,
-    duration: 50,
-    x: "-1800px",
-    ease: "linear",
-})
+  repeat: -1,
+  duration: 50,
+  x: "-1800px",
+  ease: "linear",
+});
 
 tl.to(TRAIN, {
   repeat: -1,
